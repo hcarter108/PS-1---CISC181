c9cf3ac173cfa398944e791ddadeed3435262c14
Hayden <Hayden@Machine>
2017-02-13 19:38:46 -0500

    Edited

--- src/pkgMain/cMain.java
+++ src/pkgMain/cMain.java
@@ -1,8 +1,9 @@
 package pkgMain;
 
 public class cMain {
-
 	public static void main(String[] args) {
+		
+		// Hayden Carter, Git Hub account hcarter108
 
 		Rectangle rec = new Rectangle(1.5, 2.5);		
 		System.out.println("Rectangle Area: " + rec.Area());
